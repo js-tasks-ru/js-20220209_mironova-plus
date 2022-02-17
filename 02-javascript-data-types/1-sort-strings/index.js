a01bd32acdf5242d1c1ac12c5a2650d88c405ba6
OlgaMironova <mironova_plus@mail.ru>
2022-02-17 21:12:19 +0300

    2 task.First problem solution

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,18 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
+
 export function sortStrings(arr, param = 'asc') {
 
+    function compare(a,b) {
+        return a.localeCompare(b, ['ru', 'en'], {caseFirst: 'upper'});
+    }   
+
+    if (param === 'asc')
+        return arr.slice().sort(compare);
+
+    else if (param === 'desc')
+        return arr.slice().sort(compare).reverse();
+    
+    else return 0; 
 }
