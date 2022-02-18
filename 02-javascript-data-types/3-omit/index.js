08e506eeaf7a8f05267a2f3603cd08a37a986f24
OlgaMironova <mironova_plus@mail.ru>
2022-02-18 23:46:10 +0300

    2 task. omit problem solution

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -6,4 +6,26 @@
  */
 export const omit = (obj, ...fields) => {
 
+  let tempArray = [];
+  const originArray = Object.entries(obj);
+  let array = originArray.slice();
+
+  function checkExist(val, array) {
+    let tempArray = [];
+    let index = 0;
+
+    for(let i = 0; i < array.length; i++){
+      if(array[i][0] !== val[1]) {
+        tempArray[index] = array[i];
+        index++;
+      }
+    }
+    return (tempArray.length !== array.length) ? tempArray : array; 
+  }
+
+  for (let val of Object.entries(fields)) {
+    array = checkExist(val, array);
+  }
+  
+  return (array.length !== originArray.length) ? Object.fromEntries(array) : obj;
 };
