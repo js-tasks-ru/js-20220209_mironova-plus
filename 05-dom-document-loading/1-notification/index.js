cbca321509f4fddfc245bcfc49efcbc846b66776
Olga <mironova_plus@mail.ru>
2022-02-25 12:20:55 +0300

    5 task. 1 problem

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,60 @@
 export default class NotificationMessage {
+  timerId = 0;
+  constructor (msg = '',{duration = 0, type = 'success'} = {}) {
 
+    this.msg = msg;
+    this.type = type;
+    this.duration = duration;
+
+    this.render ();
+  }
+
+  render () {
+    const element = document.createElement('div');
+    element.innerHTML = this.getTemplate();
+
+    this.element = element.firstElementChild;
+  }
+
+  getTemplate() {
+    return `
+              <div class="notification ${this.type}" style="--value: ${this.duration}s">
+                <div class="timer"></div>
+                <div class="inner-wrapper">
+                  <div class="notification-header">${this.type}</div>
+                  <div class="notification-body">
+                    ${this.msg}
+                  </div>
+                </div>
+              </div>
+            `;
+  }
+
+  show (elem = document.body) {
+
+    let el = document.querySelector('.notification');
+
+    if(el) {
+      el.remove();
+      clearTimeout(this.timerId);
+    }
+
+    elem.append(this.element);
+
+    this.timerId = setTimeout(() => {this.remove(); this.destroy();}, this.duration);
+  }
+
+  remove () {
+    document.querySelector('.notification').remove();
+  }
+
+  destroy () {
+    this.element.remove();
+    this.timerId = 0;
+    this.msg = '';
+    this.type = 'success';
+    this.duration = 0;
+  }
 }
+
+
